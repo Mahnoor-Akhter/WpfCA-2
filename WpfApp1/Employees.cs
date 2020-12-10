eeb00906c16a54a5fd0f9f7d7651c00fcd3c00a0
Mah Noor <S00199547@mail.itsligo.ie>
2020-12-10 00:08:25 +0000

    create empolyees class having details

---
+++ WpfApp1/Employees.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+	public class Employees
+	{
+		public string FirstName { get; set; }
+
+
+		public string LastName { get; set; }
+
+		public string Time { get; set; }
+		
+
+
+		public Employees(string firstname,string lastname,string time )
+		{
+			FirstName = firstname;
+			LastName = lastname;
+			Time = time;
+
+
+		}
+		public override string ToString()
+		{
+			return string.Format($"{LastName},{FirstName},{Time}");
+		}
+
+
+
+
+
+
+
+	}
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,64 @@ namespace WpfApp1
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		ObservableCollection<Employees> Employees = new ObservableCollection<Employees>();
+
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+
+			//string [] names= {"jane","joe","jones","jess"};
+			Employees e1 = new Employees("Jane", "Jones", "Part Time");
+			Employees e2 = new Employees("Joe", "Murphy", "Full Time");
+			Employees e3 = new Employees("John", "Smith", "Part Time");
+			Employees e4 = new Employees("Jess", "Walsh", "Full Time");
+
+			Employees.Add(e1);
+			Employees.Add(e2);
+			Employees.Add(e3);
+			Employees.Add(e4);
+
+			lbxName.ItemsSource = Employees;
+
+
+
+		}
+
+		private void tbxfirstname_GotFocus(object sender, RoutedEventArgs e)
+		{
+			tbxfirstname.Clear();
+		}
+
+		private void btxaddname_Click(object sender, RoutedEventArgs e)
+		{
+			//read details from screen
+			string firstName = tbxfirstname.Text;
+			string lastName = tbxlastname.Text;
+			string time = rbfulltime.Name;
+
+			//create employees object
+			Employees employees = new Employees(firstName, lastName, time);
+			//add to observalbe collection
+			Employees.Add(employees);
+
+
+
+			//refresh display manually
+			//lbxNames.ItemsSource = null;
+			//lbxNames.ItemsSource = names;
+
+		}
+
+
+
+		private void tbxlastname_GotFocus(object sender, RoutedEventArgs e)
+		{
+			tbxlastname.Clear();
 		}
 	}
 }
